32783e1dc6961f8841a5df8c3456fdc0fea093a6
jjnietoc <juanjonietocastillo@gmail.com>
2023-08-14 19:01:44 +1000

    change: create sdl2 namespace, put sdl2 funcs in namespace as objects for global use

--- display.h
+++ display.h
@@ -2,28 +2,80 @@
 #define DISPLAY_H_
 
 #include <SDL2/SDL.h>
+#include <stdexcept>
 
-class Window
-{
+namespace sdl2 {
+
+  class Window {
   public: 
-    Window() {
-      SDL_Init(SDL_INIT_VIDEO);
-      SDLWindow = SDL_CreateWindow(
-        "Chip8", 
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        500, 300,
-        0
-      );
-      SDLWindowSurface = SDL_GetWindowSurface(SDLWindow);
+    Window(int x, int y, int w, int h, bool full_screen = false) 
+    {
+      nWindow = SDL_CreateWindow("", x, y, w , h, full_screen);
+      if(nWindow == nullptr)
+        throw std::runtime_error("Unable to create window\n");  
     }
-  
-  void RenderFrame() {
-    SDL_UpdateWindowSurface(SDLWindow);
-  }  
+    inline SDL_Window* get_window() {
+      return nWindow;  
+    }
+    ~Window() {SDL_DestroyWindow(nWindow);}
+
   private:
-    SDL_Window * SDLWindow{ nullptr };
-    SDL_Surface * SDLWindowSurface { nullptr };
-};
+    SDL_Window *nWindow;
+  };
+
+  class Application {
+  public: 
+    Application() {
+      if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+      {
+        throw std::runtime_error("Unable to initialize SDL");
+      }
+    }
+    ~Application() {
+      SDL_Quit();
+    }
+  };
+
+  class Events { 
+  public:
+    bool handle_events()
+    {
+      while(SDL_PollEvent(&event))
+      {
+        if(event.type == SDL_QUIT)
+          return false;
+      }
+      return true;
+    }
+  private:
+    SDL_Event event;
+  };
+
+  class Renderer {
+  public: 
+    Renderer(SDL_Window* nWindow){
+      nRenderer = SDL_CreateRenderer(nWindow, -1, 0);
+    }
+    inline SDL_Renderer* get_renderer() {
+      return nRenderer;
+    }
+    inline void clear_screen(int r, int g, int b, int a) {
+      SDL_SetRenderDrawColor(nRenderer, r, g, b, a);
+      SDL_RenderClear(nRenderer);
+    }
+    inline void update() {
+      SDL_RenderPresent(nRenderer);
+    }
+    void render() 
+    {
+
+    }
+    ~Renderer(){
+      SDL_DestroyRenderer(nRenderer);
+    }
+  private:
+    SDL_Renderer *nRenderer;
+  };
+}
 
 #endif
