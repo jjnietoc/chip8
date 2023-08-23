ba10d727d1a39a5a42151a231090e3c668bfa3ba
jjnietoc <juanjonietocastillo@gmail.com>
2023-08-23 15:08:11 +1000

    add: const values. change: class functions

--- display.cpp
+++ display.cpp
@@ -2,22 +2,21 @@
 
 using namespace sdl2;
  
-  //  sdl2::Window window(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 64*10, 32*10);
-
+// window 
   Window::Window() 
     {
       window = SDL_CreateWindow("chip-8",
                                  SDL_WINDOWPOS_CENTERED,
                                  SDL_WINDOWPOS_CENTERED, 
-                                 64 * 10, 
-                                 32 * 10, 
+                                 WIDTH * SCALE, 
+                                 HEIGHT * SCALE, 
                                  SDL_WINDOW_SHOWN);
       if(window == nullptr)
         throw std::runtime_error("Unable to create window\n");  
     }
   Window::~Window() {SDL_DestroyWindow(window);}
 
-
+// application
   Application::Application() {
       if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
       {
@@ -29,16 +28,34 @@ using namespace sdl2;
       SDL_Quit();
     }
 
+// renderer
   Renderer::Renderer(SDL_Window* window){
       renderer = SDL_CreateRenderer(window,
                                      -1,                                  
-                                     SDL_RENDERER_ACCELERATED); 
+                                     SDL_RENDERER_ACCELERATED);
+      SDL_RenderSetScale(renderer,
+                         SCALE, 
+                         SCALE);
 }
+
+void Renderer::draw(SDL_Texture* texture, uint32_t pixels[WIDTH*SCALE]) {
+      SDL_UpdateTexture(texture,
+                        NULL,
+                        pixels,
+                        WIDTH*sizeof(u_int32_t));
+      SDL_RenderCopy(renderer,
+                     texture,
+                     NULL,
+                     NULL);
+      SDL_RenderPresent(renderer);
+    }
+
 
   Renderer::~Renderer(){
       SDL_DestroyRenderer(renderer);
     }
 
+// texture
   Texture::Texture(SDL_Renderer* renderer){
     texture = SDL_CreateTexture(renderer,
                                  SDL_PIXELFORMAT_ARGB8888,
--- display.h
+++ display.h
@@ -4,43 +4,36 @@
 #include <SDL2/SDL.h>
 #include <stdexcept>
 
-namespace sdl2 {
- 
-  class Window {
-  public: 
-    Window();
-    inline SDL_Window* get_window() {
-      return window;  
-    }
-    ~Window();
+const int WIDTH = 64;
+const int HEIGHT = 32;
+const int SCALE = 10;
 
-  private:
-    SDL_Window *window;
-  };
+namespace sdl2 {
 
   class Application {
   public: 
     Application();
     ~Application(); 
   };
+ 
+  class Window {
+  public: 
+    Window();
+    inline SDL_Window* get_window() { return window; }
+    ~Window();
+
+  private:
+    SDL_Window *window;
+  };
 
   class Renderer {
   public: 
     Renderer(SDL_Window* window);
-    inline SDL_Renderer* get_renderer() {
-      return renderer;
-    }
-    inline void update() {
-      SDL_RenderPresent(renderer);
-    }
-
-    inline void draw(SDL_Window* window, SDL_Texture* texture, uint32_t pixels[64*32]) {
-      SDL_UpdateTexture(texture, NULL, pixels, 64*sizeof(u_int32_t));
-      SDL_RenderCopy(renderer, texture, NULL, NULL);
-      SDL_RenderPresent(renderer);
-    }
-
     ~Renderer();
+
+    inline SDL_Renderer* get_renderer() { return renderer; }
+    inline void update() { SDL_RenderPresent(renderer); }
+    void draw(SDL_Texture* texture, uint32_t pixels[WIDTH*SCALE]);
 
   private:
     SDL_Renderer *renderer;
@@ -49,9 +42,7 @@ namespace sdl2 {
   class Texture {
   public:
     Texture(SDL_Renderer* renderer);
-    inline SDL_Texture* get_texture() {
-      return texture;
-  }
+    inline SDL_Texture* get_texture() { return texture; }
     ~Texture(){
 
     }
