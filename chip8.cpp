7319a914594273aad154a645284cfaf8ed2c8b1b
jjnietoc <juanjonietocastillo@gmail.com>
2023-08-24 08:18:05 +1000

    add: instruction descriptions. change: made functions of for-loops for clearing stack and display

--- chip8.cpp
+++ chip8.cpp
@@ -1,4 +1,3 @@
-#include <_types/_uint8_t.h>
 #include <iostream>
 #include <fstream>
 #include <iterator>
@@ -11,6 +10,7 @@
 
 
 Chip8::Chip8(){
+  // init all values to starting positions
   I = 0;
   pc = 512;
   sp = 0;
@@ -34,27 +34,33 @@ Chip8::Chip8(){
       0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
       0xF0, 0x80, 0xF0, 0x80, 0x80  // F
   };
+
   load_font();
-
-  // clear stack
-  for(int i = 0; i < SIZE; i++)
-  {
-    stack[i] = 0;
-    V[i] = 0;
-  }
-
-  // clear display
-  for(int i = 0; i < WIDTH * SCALE; i++)
-  {
-    display[i] = 0;
-  }
-
+  clear_stack();
+  clear_display();
 }
 
 void Chip8::load_font()
 {
   for (int i = 0; i < 80; i++)
     memory[i] = font[i];
+}
+
+void Chip8::clear_stack()
+{
+  for(int i = 0; i < SIZE; i++)
+  {
+    stack[i] = 0;
+    V[i] = 0;
+  }
+}
+
+void Chip8::clear_display()
+{
+  for(int i = 0; i < WIDTH * SCALE; i++)
+  {
+    display[i] = 0;
+  }
 }
 
 void Chip8::load_rom(std::string const& path)
@@ -78,7 +84,7 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
   u_int16_t nnn = opcode & 0x0FFF;
   u_int8_t kk = opcode & 0x0FF;
 
-  // Opcodes
+  // Decode and execute
   switch(opcode & 0xF000)
   {
     case(0x0000):
@@ -94,54 +100,54 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
         }
       break;
 
-    case(0x1000): // jump
+    case(0x1000):   // jump
       pc = nnn;
       break;
 
-    case(0x2000): // call subroutine at mem location nnn
+    case(0x2000):   // call subroutine at mem location nnn
       stack[sp] = pc;
       sp++;
       pc = nnn;
       break; 
 
-    case(0x3000): // skip if V[x] == kk
+    case(0x3000):   // skip if V[x] == kk
       if(V[x] == kk) 
         pc += 2;
       break;
 
     case(0x4000):
-      if(V[x] != kk) // skip if V[x] != kk
+      if(V[x] != kk)  // skip if V[x] != kk
         pc += 2;
       break;
 
     case(0x5000):
-      if(V[x] == V[y]) // skip if V[x] == V[y]
+      if(V[x] == V[y])  // skip if V[x] == V[y]
         pc += 2;
       break;
   
-    case(0x6000): // set register vx
+    case(0x6000):   // set register vx
       V[x] = kk;
       break;
 
-    case(0x7000): // add value to register vx
+    case(0x7000):   // add value to register vx
       V[x] += kk;
       break;
 
     case(0x8000):
       switch(opcode & 0x000F) {
-        case(0x0000):
+        case(0x0000):   // set Vx to Vy
           V[x] = V[y]; 
           break;
-        case(0x0001):
+        case(0x0001):   // bitwise OR between Vx and Vy
          V[x] |= V[y];
          break;
-        case(0x0002):
+        case(0x0002):   // bitwise AND between Vx and Vy
           V[x] &= V[y];
           break;
-        case(0x0003):
+        case(0x0003):   // bitwise XOR between Vx and Vy
           V[x] ^= V[y];
           break;
-        case(0x0004):
+        case(0x0004):   // add Vy to Vx, set VF to carry
           uint16_t result;
           result = V[x] + V[y];
           V[x] = result & 0xFF;
@@ -151,7 +157,7 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
             V[0xF] = 0;
           }
           break;
-        case(0x0005):
+        case(0x0005):     // substract Vy from Vx, set VF to not borrow
           if(V[y] > V[x]) {
             V[0xF] = 0;
           } else {
@@ -159,11 +165,11 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
           }
           V[x] -= V[y];
           break;
-        case(0x0006):
+        case(0x0006):    // shift Vx right by one
           V[0xF] = V[x] & 0x1;
           V[x] >>= 1;
           break;
-        case(0x0007):
+        case(0x0007):   // substract Vy from Vx, Vf set to not borrow
           if(V[x] > V[y]) {
             V[0xF] = 0;
           } else {
@@ -171,13 +177,13 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
           }
           V[x] = V[y] - V[x];
           break;
-        case(0x000E):
-          V[0xF] = V[x] >> 15;
+        case(0x000E):     // Vx shift left by one and VF set to the value of most signifcant bit
+          V[0xF] = V[x] >> 7;
           V[x] <<= 1;
           break;
       }
     break;
-    case(0x9000):
+    case(0x9000):   // skip if Vx != Vy
       if(V[x] != V[y])
         pc += 2;
       break;
@@ -186,14 +192,14 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
       I = nnn;
       break;
 
-    case(0xB000):
+    case(0xB000):   // jump to location nnn + V[0]
       pc = nnn + V[0];
       break;
 
     case(0xC000):
-      V[x] = (std::rand() % (0xFF)) & kk;
+      V[x] = (std::rand() % (0xFF)) & kk;   // set Vx to random byte AND kk
       break;
-    case(0xD000): // draw pixel at specified x, y location`
+    case(0xD000): // draw pixel at specified x, y location
       V[15] = 0;
 
       for(int i = 0; i < n; i++) {
@@ -209,23 +215,23 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
             } else {
               pixels[index] = 0xFFFFFFFF;
             }
-            r->draw(t->get_texture(), pixels);
           }
         }
       }
-        break;
+      r->draw(t->get_texture(), pixels);
+      break;
 
     case(0xF000):
       switch(opcode & 0x00FF) {
-        case(0x0007):
+        case(0x0007):   // set Vx to delay timer
           V[x] = delay_timer;
           break;
 
-        case(0x0015):
+        case(0x0015):   // set delay timer to Vx
           delay_timer = V[x];
           break;
 
-        case(0x001E):
+        case(0x001E):   // set I = I + Vx
           if(I + V[x] > 0xFFF)
             V[0xF] = 1;
           else
@@ -233,26 +239,26 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
           I += V[x];
           break;
         
-        case(0x0018):
+        case(0x0018):   // set sound timer to Vx
           sound_timer = V[x];
           break;
 
-        case(0x0029):
+        case(0x0029):   // set I to the location of the hex sprite of Vx
           V[I] = V[x] * 0x5;
           break;
 
-        case(0x0033):
+        case(0x0033):   // store the 100, 10, 1 representation of Vx into memory
           memory[I] = V[x] / 100;
           memory[I + 1] = (V[x] / 10) % 10;
           memory[I + 2] = V[x] % 10;
           break;
 
-        case(0x0055):
+        case(0x0055):   // store registers V0 to Vx in memory starting at I
           for(uint8_t i = 0; i <= x; i++)
             memory[I + i] = V[i];
           break;
 
-        case(0x0065):
+        case(0x0065):   // read registers V0 to Vx from memory startin at I
           for(uint8_t i = 0; i <= x; i++)
             V[i] = memory[I + i];
           break;
@@ -261,6 +267,10 @@ void Chip8::cycle(sdl2::Renderer *r, sdl2::Texture *t)
   }
   if(delay_timer > 0)
     --delay_timer;
+  if(sound_timer > 0)
+    if(sound_timer == 1)
+      // sdl_sound;
+  --sound_timer;
 }
 
 // destructor
